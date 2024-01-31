96ee4993b8c1acf96ec2d2b393b738e5b9d23c73
Nahuel Luca <nahuelluca20@gmail.com>
2024-01-31 16:26:33 -0300

    Remove unused code and update types in remix.env.d.ts

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,12 +1,3 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="@cloudflare/workers-types" />
-import type { AppLoadContext as OriginalAppLoadContext } from "@remix-run/server-runtime";
-
-declare module "@remix-run/server-runtime" {
-  export interface AppLoadContext extends OriginalAppLoadContext {
-    env: {
-      SESSION_SECRET: string;
-    };
-  }
-}
--- server.ts
+++ server.ts
@@ -1,17 +1,6 @@
 import { logDevReady } from "@remix-run/cloudflare";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
-import { z } from "zod";
-
-export const AppEnvSchema = z.object({
-  SESSION_SECRET: z.string(),
-});
-
-declare module "@remix-run/cloudflare" {
-  interface AppLoadContext {
-    env: z.output<typeof AppEnvSchema>;
-  }
-}
 
 if (process.env.NODE_ENV === "development") {
   logDevReady(build);
@@ -19,10 +8,7 @@ if (process.env.NODE_ENV === "development") {
 
 export const onRequest = createPagesFunctionHandler({
   build,
-  getLoadContext: (context) => {
-    const env = AppEnvSchema.parse(context.env);
-    return { env };
-  },
+  getLoadContext: (context) => ({ env: context.env }),
   mode: build.mode,
   // mode: process.env.NODE_ENV,
 });
