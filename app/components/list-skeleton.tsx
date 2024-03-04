9b021a631ea17c4659eb4b94893a4a5ad4e27ea7
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-04 12:57:44 -0300

    Update list skeleton styles

--- app/components/list-skeleton.tsx
+++ app/components/list-skeleton.tsx
@@ -3,10 +3,10 @@ export default function ListSkeleton() {
   return (
     <ul className="list-disc space-y-2 w-full">
       {items.map((item, index) => (
-        <li key={index} className="w-full">
+        <li key={index} className="w-full h-[24px]">
           <div className="animate-pulse flex space-x-4 w-full">
-            <div className="grid grid-cols-3 gap-4 w-full">
-              <div className="h-[24px] bg-slate-400 py-1 w-full rounded-md col-span-2"></div>
+            <div className="w-full">
+              <div className="h-2 bg-slate-400 py-1 w-full rounded-md col-span-2"></div>
             </div>
           </div>
         </li>
