278dbe3a08999fbd55ba0c845cf1c77ad91c8a1e
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-04 13:48:52 -0300

    Update list item width in ListSkeleton component

--- app/components/list-skeleton.tsx
+++ app/components/list-skeleton.tsx
@@ -3,7 +3,7 @@ export default function ListSkeleton() {
   return (
     <ul className="list-disc space-y-2 w-full">
       {items.map((item, index) => (
-        <li key={index} className="w-full h-[24px]">
+        <li key={index} className="w-[240px] md:w-[300px] h-[24px]">
           <div className="animate-pulse flex space-x-4 w-full">
             <div className="w-full">
               <div className="h-2 bg-slate-400 py-1 w-full rounded-md col-span-2"></div>
