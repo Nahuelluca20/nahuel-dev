08e2d2f44c6d4f07d55c5d20af0f6073a2d54726
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-04 12:51:01 -0300

    Update list skeleton height

--- app/components/list-skeleton.tsx
+++ app/components/list-skeleton.tsx
@@ -6,7 +6,7 @@ export default function ListSkeleton() {
         <li key={index} className="w-full">
           <div className="animate-pulse flex space-x-4 w-full">
             <div className="grid grid-cols-3 gap-4 w-full">
-              <div className="h-[16px] bg-slate-500 py-1 w-full rounded col-span-2"></div>
+              <div className="h-[20px] bg-slate-400 py-1 w-full rounded col-span-2"></div>
             </div>
           </div>
         </li>
