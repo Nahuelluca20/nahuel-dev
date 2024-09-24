d817a858cc7c4221bc522d0c5bfa08270cf8b335
Nahuel Luca <nahuelluca20@gmail.com>
2024-09-23 22:56:50 -0300

    chore: Add comment to getBlogPost function

--- app/routes/blog.$slug/queries.ts
+++ app/routes/blog.$slug/queries.ts
@@ -2,6 +2,7 @@ import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
 import { post } from "~/db/schema";
 
+// back
 export async function getBlogPost(env: D1Database, slug: string) {
   const db = drizzle(env);
   return await db
